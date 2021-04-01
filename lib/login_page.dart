6bee1b2fa88a9ab869862cf86e46f50558295951
PURPLE <9707.hyeon@gmail.com>
2021-04-01 16:56:16 +0900

    Update

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("login page")
+
+    Size screenSize = MediaQuery.of(context).size;
+    double width = screenSize.width;
+    double height = screenSize.height;
+
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("로그인", style: TextStyle(color: Colors.black)),
+          backgroundColor: Colors.white38,
+          leading: Container(),
+        ),
+
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children : <Widget>[
+            Center(
+              child:
+                TextFormField(
+                  decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: "아이디입력"
+                  ),
+                ),
+            ),
+            Center(
+              child:
+                ElevatedButton(child: Text("로그인"))
+                  
+            )
+          ]
+        )
+          
+      ),
     );
   }
+
 }
--- lib/root_page.dart
+++ lib/root_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hybrid_access_app/login_page.dart';
 import 'tab_page.dart';
+import 'login_page.dart';
 
 class RootPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return TabPage();
+    return LoginPage();
   }
 }
